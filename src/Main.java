fc724365317c9518a2bd3b315dbf977a6ffb6b32
AngelsGV <angels.not99@gmail.com>
2024-04-21 19:21:33 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
+        System.out.println(leerEntero());
+    }
     static Integer leerEntero() {
         Integer resultado;
         while (true) {
